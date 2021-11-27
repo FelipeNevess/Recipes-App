db857b4d5f7d754b79147c3bd16fdd2f1956e631
FelipeNevess <felipe012neves@gmail.com>
2021-11-27 13:13:26 -0300

    adicionando component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
+import HeaderSearchBar from './HeaderSearchBar';
+import '../styles/Header.css';
+
+function Header({ title, hasSearchIcon, page }) {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const history = useHistory();
+
+  const handleClickSearchBar = () => {
+    setShowSearchBar(!showSearchBar);
+  };
+
+  const renderButton = () => (
+    <button type="button" onClick={ handleClickSearchBar } className="header-buttons">
+      <img src={ searchIcon } alt="search Icon" data-testid="search-top-btn" />
+    </button>
+  );
+
+  const handleClickProfile = () => {
+    history.push('/perfil');
+  };
+
+  return (
+    <>
+      <header className="header-container">
+        <button type="button" onClick={ handleClickProfile } className="header-buttons">
+          <img src={ profileIcon } alt="search Icon" data-testid="profile-top-btn" />
+        </button>
+        <h1 data-testid="page-title" className="header-title">{ title }</h1>
+        { hasSearchIcon && renderButton()}
+        { !hasSearchIcon && <div /> }
+      </header>
+
+      { showSearchBar && <HeaderSearchBar
+        page={ page }
+        handleClickSearchBar={ handleClickSearchBar }
+      /> }
+    </>
+  );
+}
+
+export default Header;
